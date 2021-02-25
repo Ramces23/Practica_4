60b131c4b4e584934f28ed02248c0369d0b02e8e
ramces batista peña <ramcesbape@gmail.com>
2021-02-25 14:58:04 -0400

    Entrada Ejercicio_5

---
+++ Ejercicio_5/Ejercicio_5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ejercicio_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa desarrollado por Ramces Batista Peña para almacenar nombres y mostrarlos en una lista ");
+            string[] Nombres = new string[100];
+            Console.WriteLine("Escriba sus nombres y presione enter en blanco para concluir ");
+            int i = -1;
+
+            do
+            {
+                i++;
+                Nombres[i] = Console.ReadLine();
+            } while (Nombres[i].Length !=0 );
+
+            Console.Clear();
+            Console.WriteLine("Estos son los nombres: ");
+            i = -1;
+
+            do
+            {
+                i++;
+                Console.WriteLine(Nombres[i]);
+            } while (Nombres[i].Length != 0);
+        }
+    }
+}

e0bd99950bd4d15d7a8362bd5bda75b9ec3e3f8b
ramces batista peña <ramcesbape@gmail.com>
2021-02-25 09:08:45 -0400

    Entrada de ejercicio 1

---
+++ Ejercicio_1/Ejercicio_1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa desarrollado por Ramces Batista Peña para guardar cuatro numeros digitados por el teclado y luego mostrar su media y los numeros digitados");
+
+            int[] Numeros = new int[4];
+            float N = 0;
+            Console.WriteLine("Presione cualquier tecla para continuar ");
+            Console.ReadKey();
+            Console.Clear();
+
+
+            for (int i = 0; i <= 3; i++)
+            {
+                int M = 1 + i;
+                Console.WriteLine("Digite el numero {0} ", M);
+                Numeros[i] = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+
+
+            }
+            for (int i = 0; i <= 3; i++)
+            {
+                N = N + Numeros[i];
+            }
+            N /= 4;
+            Console.WriteLine("La media aritmetica es: " + N);
+            
+            for (int i = 0; i <= 3; i++)
+            {
+                int M = 1 + i;
+                Console.WriteLine("[" + Numeros[i] + "] " +   " Este es el numero " + M);
+            }
+
+
+
+
+        }
+    }
+}

15bf51d28cdf850d76ffd88312717e7dad43017e
ramces batista peña <ramcesbape@gmail.com>
2021-03-01 03:53:56 -0400

    Entrada Ejercicio 9

---
+++ Ejercicio_9/Ejercicio_9/C-TablaMulti.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_9
+{
+    public class C_TablaMulti
+    {
+        public C_TablaMulti() {
+            Numero = 0;
+            Multiplicador = 0;
+        
+        
+        }
+        public int Numero;
+        public int Multiplicador;
+        public void Multiplicar()
+        {
+            do
+            {
+                Console.WriteLine("Introdusca su numero, introdusca -1 para cerrar el programa");
+                Numero = int.Parse(Console.ReadLine());
+                for (Multiplicador = 1; Multiplicador <= 12; Multiplicador++)
+                {
+                    Console.WriteLine(Numero + "*" + Multiplicador + "=" + Numero * Multiplicador + "\n");
+
+                }
+                Console.WriteLine("Presione enter para continuar ");
+                Console.ReadKey();
+                Console.Clear();
+            } while (Numero != -1);
+        }
+
+    }
+}
---
+++ Ejercicio_9/Ejercicio_9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Ejercicio_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa desarrollado por Ramces Batista Peña para calcular la tabla de multiplicar de numeros ");
+            C_TablaMulti TablaMulti;
+            TablaMulti = new C_TablaMulti();
+            TablaMulti.Multiplicar();
+
+
+        }
+    }
+}
